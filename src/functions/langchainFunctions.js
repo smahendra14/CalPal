e241e04586d91c90a0f5ab68eaf6d286a7066071
smahendra14 <satvik.mahendra@gmail.com>
2024-08-22 15:51:59 -0500

    fixed prompt for more accuracy

--- src/functions/langchainFunctions.js
+++ src/functions/langchainFunctions.js
@@ -2,14 +2,15 @@ import { ChatOpenAI } from "@langchain/openai"
 import {ChatPromptTemplate} from "@langchain/core/prompts";
 import {StructuredOutputParser} from 'langchain/output_parsers'
 import {z} from 'zod';
+import { key } from "./api.js";
 
 const model = new ChatOpenAI({
     modelName: "gpt-3.5-turbo",
     temperature: 0,
+    openAIApiKey: key
 });
 
 export async function extractEventInfo(description) {
-    console.log("testing this function. the event description is: " + description);
     const response = await callZodOutputParser(description);
     return response;
 }
@@ -23,11 +24,11 @@ async function callZodOutputParser(description) {
   
     const outputParser = StructuredOutputParser.fromZodSchema(
       z.object({
-        title: z.string().describe("the title of the event occurring"),
+        title: z.string().describe("the title of the event occurring. capitalize words as necessary"),
         startTime: z
           .string()
           .describe(
-            "the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30."
+            "the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30. If an event is 4 hours from now, the start time would be 4 hours after the current time. Also, as an example, if an event is described as being in 30 minutes, the start time is 30 minutes after the current time"
           ),
         endTime: z
           .string()
@@ -48,18 +49,12 @@ async function callZodOutputParser(description) {
       })
     );
 
-  
     const currentDate = new Date();
     const tomorrow = new Date();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
     tomorrow.setDate(currentDate.getDate() + 1);
-    const promptDescription = description + ". the date today is " + currentDate.toDateString();
-    const phrase =
-      description.concat(
-        currentDate.toDateString()
-      );
-  
-    console.log(phrase);
-    console.log(promptDescription);
+    const phrase = description + ". the date today is " + currentDate.toDateString() + `. the time right now is ${hours}:${minutes}`;
   
     const chain = prompt.pipe(model).pipe(outputParser);
     return await chain.invoke({
