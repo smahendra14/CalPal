7293c7cf0e31c5b04d90a62fabb9810c5e0fcbbc
smahendra14 <satvik.mahendra@gmail.com>
2024-12-16 18:32:21 -0600

    update supabase info and switch to gemini

--- src/functions/langchainFunctions.js
+++ src/functions/langchainFunctions.js
@@ -1,67 +1,72 @@
-import { ChatOpenAI } from "@langchain/openai"
-import {ChatPromptTemplate} from "@langchain/core/prompts";
-import {StructuredOutputParser} from 'langchain/output_parsers'
-import {z} from 'zod';
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StructuredOutputParser } from "langchain/output_parsers";
+import { z } from "zod";
 import { key } from "./api.js";
 
-const model = new ChatOpenAI({
-    modelName: "gpt-3.5-turbo",
-    temperature: 0,
-    openAIApiKey: key
+const model = new ChatGoogleGenerativeAI({
+  modelName: "gemini-pro",
+  temperature: 0,
+  apiKey: key,
 });
 
 export async function extractEventInfo(description) {
-    const response = await callZodOutputParser(description);
-    return response;
+  const response = await callZodOutputParser(description);
+  return response;
 }
 
 async function callZodOutputParser(description) {
-    const prompt = ChatPromptTemplate.fromTemplate(`
+  const prompt = ChatPromptTemplate.fromTemplate(`
       Extract information from the following phrase. 
       Formatting instructions: {format_instructions}
       Phrase: {phrase}
     `);
-  
-    const outputParser = StructuredOutputParser.fromZodSchema(
-      z.object({
-        title: z.string().describe("the title of the event occurring. capitalize words as necessary"),
-        startTime: z
-          .string()
-          .describe(
-            "the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30. If an event is 4 hours from now, the start time would be 4 hours after the current time. Also, as an example, if an event is described as being in 30 minutes, the start time is 30 minutes after the current time"
-          ),
-        endTime: z
-          .string()
-          .describe(
-            "one hour after the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30"
-          ),
-        date: z.string().describe("the date of the event"),
-        calendarStartInputTime: z
-          .string()
-          .describe(
-            "The date and time at which the event is starting in ISO format: YYYY-MM-DDThh:mm:ss"
-          ),
-        calendarEndInputTime: z
-          .string()
-          .describe(
-            "The date and time at which the event is ending in ISO format: YYYY-MM-DDThh:mm:ss"
-          ),
-      })
-    );
 
-    const currentDate = new Date();
-    const tomorrow = new Date();
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    tomorrow.setDate(currentDate.getDate() + 1);
-    const phrase = description + ". the date today is " + currentDate.toDateString() + `. the time right now is ${hours}:${minutes}`;
-  
-    const chain = prompt.pipe(model).pipe(outputParser);
-    return await chain.invoke({
-      phrase: phrase,
-      format_instructions: outputParser.getFormatInstructions(),
-    });
-  }
+  const outputParser = StructuredOutputParser.fromZodSchema(
+    z.object({
+      title: z
+        .string()
+        .describe(
+          "the title of the event occurring. capitalize words as necessary"
+        ),
+      startTime: z
+        .string()
+        .describe(
+          "the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30. If an event is 4 hours from now, the start time would be 4 hours after the current time. Also, as an example, if an event is described as being in 30 minutes, the start time is 30 minutes after the current time"
+        ),
+      endTime: z
+        .string()
+        .describe(
+          "one hour after the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30"
+        ),
+      date: z.string().describe("the date of the event"),
+      calendarStartInputTime: z
+        .string()
+        .describe(
+          "The date and time at which the event is starting in ISO format: YYYY-MM-DDThh:mm:ss"
+        ),
+      calendarEndInputTime: z
+        .string()
+        .describe(
+          "The date and time at which the event is ending in ISO format: YYYY-MM-DDThh:mm:ss"
+        ),
+    })
+  );
 
+  const currentDate = new Date();
+  const tomorrow = new Date();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  tomorrow.setDate(currentDate.getDate() + 1);
+  const phrase =
+    description +
+    ". the date today is " +
+    currentDate.toDateString() +
+    `. the time right now is ${hours}:${minutes}`;
 
-
+  const chain = prompt.pipe(model).pipe(outputParser);
+  return await chain.invoke({
+    phrase: phrase,
+    format_instructions: outputParser.getFormatInstructions(),
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from "./App.js"
-import reportWebVitals from './reportWebVitals.js';
-import { createClient } from '@supabase/supabase-js';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals.js";
+import { createClient } from "@supabase/supabase-js";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 const supabase = createClient(
-  "https://zoyjtlhkvaswvfbicynf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpveWp0bGhrdmFzd3ZmYmljeW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyODkxMzQsImV4cCI6MjAzMjg2NTEzNH0.xAsjt7g1pc2j0AXvhk5ihtMWYNBepvuX0HGd6nVFwGw"
+  "https://aoapofprbeqinxzapgku.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvYXBvZnByYmVxaW54emFwZ2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzOTQwODksImV4cCI6MjA0OTk3MDA4OX0.WmZqrRXiLysBQz5h8N2I56RztW15yIuWmrClkOwdc-4"
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <SessionContextProvider supabaseClient={supabase}> 
+  <SessionContextProvider supabaseClient={supabase}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
