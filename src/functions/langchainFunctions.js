5ec1d0bbf44cbc3f71a8c474ff4deee49ef405e1
smahendra14 <satvik.mahendra@gmail.com>
2024-12-16 20:12:05 -0600

    modify prompt to default to future start times

--- src/functions/langchainFunctions.js
+++ src/functions/langchainFunctions.js
@@ -31,7 +31,7 @@ async function callZodOutputParser(description) {
       startTime: z
         .string()
         .describe(
-          "the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30. If an event is 4 hours from now, the start time would be 4 hours after the current time. Also, as an example, if an event is described as being in 30 minutes, the start time is 30 minutes after the current time"
+          "the start time of the event. Convert times to military time based off a 24 hour clock. For example, 2 pm would be 14:00 and 10:30 am would be 10:30. If an event is 4 hours from now, the start time would be 4 hours after the current time. Also, as an example, if an event is described as being in 30 minutes, the start time is 30 minutes after the current time. unless specified otherwise, start times should be in the future from the current date and time"
         ),
       endTime: z
         .string()
